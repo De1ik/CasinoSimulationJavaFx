11c8a62854baa49910ae031f6a83ab702abb4729
De1ik <olegovich.1903@gmail.com>
2024-05-10 14:35:25 +0200

    pdf documentation and jar were added

--- src/main/java/com/example/mycasinofx/Model/exceptions/AgeFailAccess.java
+++ src/main/java/com/example/mycasinofx/Model/exceptions/AgeFailAccess.java
@@ -1,13 +1,16 @@
 package com.example.mycasinofx.Model.exceptions;
 
 /**
- * The AgeFailAccess class represents an exception that is thrown when access is denied due to age restrictions.
+ * The AgeFailAccess class represents an exception
+ * that is thrown when access is denied due to age restrictions.
  */
 public class AgeFailAccess extends Exception {
 
     /**
-     * Constructs a new AgeFailAccess exception with the specified error message.
-     * @param errorMessage The error message explaining the reason for the age access failure.
+     * Constructs a new AgeFailAccess exception with
+     * the specified error message.
+     * @param errorMessage The error message explaining
+     *                     the reason for the age access failure.
      */
     public AgeFailAccess(String errorMessage) {
         super(errorMessage);
